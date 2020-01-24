a3e73cfb2d8ca4dcd50e1ecf914668bb76b87ca5
IoanDascalu <idascalu@ithaca.edu>
2020-01-24 12:46:08 -0500

    Added code for BankAccount isEmailValid

--- src/main/java/edu/ithaca/dragon/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/BankAccount.java
@@ -39,8 +39,85 @@ public class BankAccount {
         if (email.indexOf('@') == -1){
             return false;
         }
-        else {
-            return true;
+        //Splits into prefix and domain
+        String[] stringArray=email.split("@", 2);
+        if(stringArray[0].length()<1){
+            return false;
         }
+        else if(stringArray[1].length()<1){
+            return false;
+        }
+        //Checking that prefix is valid
+        for (int i=0; i<stringArray[0].length(); i++){
+            if ((Character.isDigit(stringArray[0].charAt(i))) || (Character.isLetter(stringArray[0].charAt(i))) || (stringArray[0].charAt(i)=='_') || (stringArray[0].charAt(i)=='.') || (stringArray[0].charAt(i)=='-')){
+                if((stringArray[0].charAt(i)=='_') || (stringArray[0].charAt(i)=='.') || (stringArray[0].charAt(i)=='-')){
+                    if((i+1)>=stringArray[0].length()){
+                        return false;
+                    }
+                    else{
+                        if (stringArray[0].charAt(i+1)=='_' || stringArray[0].charAt(i+1)=='.' || stringArray[0].charAt(i+1)=='-'){
+                            return false;
+                        }
+                    }
+                }
+            }
+            else{
+                return false;
+            }
+        }
+        //splitting domain into two parts
+        if(stringArray[1].indexOf('.') == -1){
+            return false;
+        }
+
+        String[] domainArray=stringArray[1].split(".", 2);
+        if(domainArray[0].length()<1){
+            return false;
+        }
+        if(domainArray[1].length()<1){
+            return false;
+        }
+
+        //checking that first part of the domain is valid
+        for (int i=0; i<domainArray[0].length(); i++){
+            if ((Character.isDigit(domainArray[0].charAt(i))) || (Character.isLetter(domainArray[0].charAt(i))) || (domainArray[0].charAt(i)=='-')){
+                if((domainArray[0].charAt(i)=='-')){
+                    if((i+1)>=domainArray[0].length()){
+                        return false;
+                    }
+                    else{
+                        if (domainArray[0].charAt(i+1)=='-'){
+                            return false;
+                        }
+                    }
+                }
+            }
+            else{
+                return false;
+            }
+        }
+        for (int i=0; i<domainArray[1].length(); i++){
+            if (domainArray[1].length()<2){
+                return false;
+            }
+            if ((Character.isDigit(domainArray[1].charAt(i))) || (Character.isLetter(domainArray[1].charAt(i))) ||  (domainArray[0].charAt(i)=='-')){
+                if (stringArray[0].charAt(i)=='-'){
+                    if((i+1)>=domainArray[0].length()){
+                        return false;
+                    }
+                    else{
+                        if (stringArray[0].charAt(i+1)=='_' || stringArray[0].charAt(i+1)=='.' || stringArray[0].charAt(i+1)=='-'){
+                            return false;
+                        }
+                    }
+                }
+            }
+            else{
+                return false;
+            }
+        }
+
+        return true;
+
     }
 }
